3933763a56342e79eb59b41f5eec28d1b618c3d1
jasnmar <jasnmar@gmail.com>
2025-04-18 16:41:01 -0400

    Progress

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 import http from "node:http"
 
 import { getDataFromDB } from "./database/db.js"
+import { sendResponse } from "./utils/sendResponse.js"
+import { send } from "node:process"
 
 const PORT = 8000
 console.log("hello node")
 
+const contPath = "/api/continent/"
+
 const server = http.createServer(async (req, res) => {
   if (req.url === "/api" && req.method === "GET") {
-    const destinatinons = await getDataFromDB()
-    res.setHeader("Content-Type", "application/json")
-    res.statusCode = 200
-    res.end(JSON.stringify(destinatinons), () =>
-      console.log("response sent")
-    )
+    const destinations = await getDataFromDB()
+    sendResponse(res, destinations)
+  } else if (req.url.startsWith(contPath) && req.method === "GET") {
+    const continent = req.url.replace(contPath, "")
+    const destinations = await getDataFromDB()
+    const filteredDesinatons = destinations.filter((destination) => {
+      return (
+        destination.continent.toLowerCase().replace(" ", "") ===
+        continent.toLowerCase()
+      )
+    })
+    sendResponse(res, filteredDesinatons)
   } else {
-    res.statusCode = 404
-    res.setHeader("Content-Type", "application/json")
-    const notFoundMessage = {error: "not found", message: "The requested route does not exist"}
-    res.end(JSON.stringify(notFoundMessage), () => console.log("404 sent"))
+    sendResponse(res, {
+      error: "not found",
+      message: "The requested route does not exist"
+    } , 404)
+
   }
 })
 
